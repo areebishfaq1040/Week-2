c7e8ace586093966a9951a61618229389fa8ca7b
areebishfaq1040 <areebishfaq1040@gmial.com>
2026-02-20 20:54:52 +0500

    week1 week2

---
+++ Week2/Task 1 (CL)/Hexagon.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+main ()
+{
+cout << "                ________________       "<<endl;
+cout << "               /                \\               "<<endl;
+cout << "              /                  \\       "<<endl;
+cout << "             /                    \\          "<<endl;
+cout << "            /                      \\     "<<endl;
+cout << "           /                        \\          "<<endl;
+cout << "           \\                        /               "<<endl;
+cout << "            \\                      /               "<<endl;
+cout << "             \\                    /               "<<endl;
+cout << "              \\                  /          "<<endl;
+cout << "               \\________________/             "<<endl;
+cout << "                                          "<<endl;
+cout << "                             "<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout << ""<<endl;
+cout<<endl;
+}
---
+++ Week2/Task 15(CP)/My Car.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+main ()
+{
+cout << "                                                    "<<endl;
+cout << "                                                        "<<endl;
+cout << "                                                     "<<endl;
+cout << "                                                              "<<endl;
+cout << "                _____________________                              "<<endl;                 
+cout << "              /|          |          \\      "<<endl;
+cout << "             / |          |            \\                              "<<endl;
+cout << "   _________/__|__________|_____________/___________________   -------------- "<<endl;
+cout << "  |                                                        /  -----------"<<endl;
+cout << "   \\          ___             (!>)             ___       /    ------ "<<endl;
+cout << "   /         /    \\                          /     \\     \\   ----------       "<<endl;
+cout << "  |_________|      |________________________|       |______\\  --------------   "<<endl;
+cout << "             \\____/                          \\_____/        "<<endl;
+cout << "                                                    "<<endl;
+cout << "                                                        "<<endl;
+cout << "                                                     "<<endl;
+cout << "                                                              "<<endl;
+cout << "                                                   "<<endl;
+cout << "                                      "<<endl;
+cout << "                                               "<<endl;
+cout << "                          "<<endl;
+cout << "                                            "<<endl;
+cout << "                               "<<endl;
+cout<<endl;
+}
